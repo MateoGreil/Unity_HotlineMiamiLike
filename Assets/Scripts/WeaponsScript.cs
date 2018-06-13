4eba8c0d128aef3b7ad2e322150e99225e6cabbc
Geoffrey NEGRI <gnegri@e3r12p12.42.fr>
2018-06-13 16:20:13 +0200

    Added weapon script

---
+++ Assets/Scripts/WeaponsScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponsScript : MonoBehaviour {
+	public GameObject	holder;
+	public GameObject	bullet;
+	public bool 		fireWeapon;
+	public int			ammos;
+	public float 		power;
+	public float 		fireRate;
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	//Fire bullet if fireWeapon. Else stab
+	void Fire () {
+		if (fireWeapon) {
+			if (ammos > 0) {
+				if (holder.tag == "Player")
+					ammos -= 1;
+				//Instantiate bullet and play bullet animation
+			}
+		}
+		else {
+			//Animate knife
+		}
+	}
+}
