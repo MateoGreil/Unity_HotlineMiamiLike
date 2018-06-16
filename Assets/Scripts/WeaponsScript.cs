0017e6ed903fcc3445c6d6c2afedad3561b1715c
Geoffrey NEGRI <gnegri@e3r9p15.42.fr>
2018-06-16 15:51:00 +0200

    Added shotgun weapon and bullets prefabs and modified weapon script to change sprite when holding

--- Assets/Scripts/WeaponsScript.cs
+++ Assets/Scripts/WeaponsScript.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponsScript : MonoBehaviour {
-	public GameObject	holder;
 	public GameObject	bullet;
 	public bool 		fireWeapon;
 	public int			ammos;
 	public float 		power;
 	public float 		fireRate;
+	public Sprite 		attachedSprite;
+	public Sprite 		onGroundSprite;
+	
+	private SpriteRenderer spriteRenderer;
+
+	void Start () {
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+	}
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,7 +25,7 @@ public class WeaponsScript : MonoBehaviour {
 	public void Fire () {
 		if (fireWeapon) {
 			if (ammos > 0) {
-				if (holder.tag == "Player")
+				if (transform.parent.tag == "Player")
 					ammos -= 1;
 				//Instantiate bullet and play bullet animation
 				bullet = Instantiate(bullet, gameObject.transform.position, Quaternion.identity, transform);
@@ -28,6 +35,15 @@ public class WeaponsScript : MonoBehaviour {
 			//Animate knife
 		}
 	}
+
+	//Change weapon's sprite when picked up
+	public void GetWeaponSprite () {
+		if (transform.parent)
+			spriteRenderer.sprite = attachedSprite;
+		else
+			spriteRenderer.sprite = onGroundSprite;		
+	}
+	
 
 	/**** FAIRE LA FONCTION POUR LA PERTE DE VIE DU PERSO ****/
 }
