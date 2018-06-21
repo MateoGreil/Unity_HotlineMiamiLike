d90ce875db0d595f4c553e6fa09a272d889f7b34
Geoffrey NEGRI <gnegri@e3r12p14.42.fr>
2018-06-21 18:00:03 +0200

    Added main menu with link on buttons and music

--- Assets/Scripts/Menu/LogoHit.cs
+++ Assets/Scripts/Menu/LogoHit.cs
@@ -7,6 +7,13 @@ public class LogoHit : MonoBehaviour {
 	private AudioSource		audio;
 	private Vector3			selectPos;
 	private RaycastHit2D		hit;
+	private Vector3 originPosition;
+	private Quaternion originRotation;
+
+	public float shake_decay = 0.1f;
+	public float shake_intensity = .3f;
+
+	private float temp_shake_intensity = 0;
 	// Use this for initialization
 	void Start () {
 		audio = GetComponent<AudioSource>();
@@ -14,25 +21,37 @@ public class LogoHit : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		/*
+		
 		if (Input.GetMouseButtonDown(0)) {
 			selectPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			Vector2 mousePos2D = new Vector2(selectPos.x, selectPos.y);
 			if (hit = Physics2D.Raycast(mousePos2D, Vector2.zero))
 			{
-				if (hit.collider != null)
+				if (hit.collider != null) {
 					audio.Play();
+					Shake ();
+				}
 				else
 					Debug.Log ("This isn't a Player");
 			}
 		}
-		*/
-		Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
-        RaycastHit2D hit=Physics2D.Raycast(rayPos, Vector2.zero, 0f);
-         
-         if (hit)
-         {
-              Debug.Log(hit.transform.name);
-        }
+
+		if (temp_shake_intensity > 0){
+			transform.position = originPosition + Random.insideUnitSphere * temp_shake_intensity;
+			transform.rotation = new Quaternion(
+				originRotation.x + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f,
+				originRotation.y + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f,
+				originRotation.z + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f,
+				originRotation.w + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f);
+			temp_shake_intensity -= shake_decay;
+		}
+		
+	}
+
+	void Shake(){
+		originPosition = transform.position;
+		originRotation = transform.rotation;
+		temp_shake_intensity = shake_intensity;
+
 	}
 }
