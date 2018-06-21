cc7d9ab154530fc23f07af0883eb1e0e3d5152f7
Geoffrey NEGRI <gnegri@e3r12p14.42.fr>
2018-06-21 11:46:16 +0200

    Added script to create a path and script to follow it

---
+++ Assets/Scripts/FollowPath.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour {
+
+	#region Enums
+	public enum MovementType //Type of movement
+	{
+		MoveTowards,
+		LerpTowards
+	}
+	#endregion
+
+	#region Public Variables
+	public MovementType Type = MovementType.MoveTowards;
+	public MovementPath MyPath;
+	public float speed = 1;
+	public float MaxDistanceToGoal = .1f; //How close does it have to be to the point to e considered at point
+	#endregion
+	
+	#region Private Variables
+	private IEnumerator<Transform> pointInPath;
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+		if (MyPath == null) {
+			Debug.LogError("MovementPath cannot be null", gameObject);
+			return;
+		}
+		//Sets up a reference to  an instance of the coroutine GetNextPathPoint
+		pointInPath = MyPath.GetNextPathPoint();
+		//Get the next point in the path to move to
+		pointInPath.MoveNext();
+		if (pointInPath.Current == null) {
+			Debug.LogError("A path must have points to follow", gameObject);
+			return;
+		}
+		//Set the position of the object to the position of the starting point
+		transform.position = pointInPath.Current.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (pointInPath == null || pointInPath.Current == null)
+			return;
+		if (Type == MovementType.MoveTowards)
+		{
+			//Move to the next point
+			transform.position = Vector3.MoveTowards(transform.position,
+				pointInPath.Current.position, Time.deltaTime * speed);
+		}
+		else if (Type == MovementType.LerpTowards)
+		{
+			//Move to the next point using lerp
+			transform.position = Vector3.Lerp(transform.position,
+				pointInPath.Current.position, Time.deltaTime * speed);
+		}
+		//Check to see if close enough to the next point using Pythagore
+		var distanceSquared =(transform.position - pointInPath.Current.position).sqrMagnitude;
+		if (distanceSquared < MaxDistanceToGoal * MaxDistanceToGoal)
+			pointInPath.MoveNext();
+	}
+}
---
+++ Assets/Scripts/MovementPath.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPath : MonoBehaviour {
+
+	#region Enums
+	public enum PathTypes
+	{
+		linear,
+		loop
+	}
+	#endregion
+
+	#region Public Variables
+	public PathTypes PathType;
+	public int movementDirection = 1; //1 = Clockwise, *1 = counter clockwise
+	public int movingTo = 0; //Used to determine which point to go to
+	public Transform[] PathSequence; //Array of the points to go through in the path
+	#endregion
+
+	public void OnDrawGizmos() {
+		//Verify if path has at leat two points
+		if (PathSequence == null || PathSequence.Length < 2)
+			return;
+		//Draw lines between points
+		for (int i = 1; i < PathSequence.Length; i++)
+			Gizmos.DrawLine(PathSequence[i-1].position, PathSequence[i].position);
+		//Draw line from last point to first if is a loop
+		if (PathType == PathTypes.loop)
+			Gizmos.DrawLine(PathSequence[0].position, PathSequence[PathSequence.Length-1].position);
+	}
+
+	public IEnumerator<Transform> GetNextPathPoint() {
+		if (PathSequence == null || PathSequence.Length < 1)
+			yield break;
+
+		while (true) //Does not infinite loop thanks to yeild return
+		{
+			//Returns current point in pathSequence and wait for next call of enumerator
+			yield return PathSequence[movingTo];
+
+			//Exit coroutine if only one point
+			if (PathSequence.Length == 1)
+				continue;
+			//If linear, go to the final point and go the other way
+			if (PathType ==PathTypes.linear)
+			{
+				if (movingTo <= 0)
+					movementDirection = 1;
+				else if (movingTo >= PathSequence.Length - 1)
+					movementDirection = -1;
+			}
+			movingTo = movingTo + movementDirection;
+			//If loop, always 
+			if (PathType == PathTypes.loop)
+			{
+				if (movingTo >= PathSequence.Length)
+					movingTo = 0;
+				//If moving backwards
+				else if (movingTo < 0)
+					movingTo = PathSequence.Length - 1;
+			}
+		}
+	}
+}
