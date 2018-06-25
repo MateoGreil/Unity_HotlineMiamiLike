f39514f18aa950a4d6acc95114e325e05233f876
MateoGreil <mateo.greil@gmail.com>
2018-06-25 15:36:44 +0200

    ajout level0

---
+++ Assets/Scripts/EndOfLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndOfLevel : MonoBehaviour {
+	GameObject[] enemy;
+ 
+	void Awake() {
+	}
+
+	void Update()
+    {
+		enemy = GameObject.FindGameObjectsWithTag("Enemy");
+        if (enemy.Length == 0) {
+        	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+    }
+}
